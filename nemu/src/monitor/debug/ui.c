3ed44abb6766c58d85824a93c6cda74892cd5ae4
tracer-ics2018 <tracer@njuics.org>
2018-09-12 08:15:26 +0000

    >  compile 171240524 jarviszly Linux 2de3c4ee0c59 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  08:15:26 up  2:18,  4 users,  load average: 0.00, 0.00, 0.00 df47a5eb77378ae099905de82f70a175719f2a91

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,8 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args);
+
 static struct {
   char *name;
   char *description;
@@ -46,7 +48,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
- // { "si", "Step Through", cmd_si},
+  { "si", "Step Through", cmd_si},
  
  
  
@@ -79,8 +81,19 @@ static int cmd_help(char *args) {
   }
   return 0;
 }
+  
 
-//static int cmd_si(char *args){};
+static int cmd_si(char *args){
+  int num;
+  if(args==NULL) 
+     num=1;
+  else 
+    {
+	num=atoi(args);
+    }  
+  cpu_exec(num);
+  return 0;
+};
 	
 
 
