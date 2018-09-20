4df06da0aa8488d35f6cfd81f9f3c0fa34e17fe7
tracer-ics2018 <tracer@njuics.org>
2018-09-20 22:49:47 +0800

    >  compile 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-1 (2018-07-05) x86_64 GNU/Linux  22:49:47 up 18:20,  5 users,  load average: 0.10, 0.21, 0.24 5f37e03c1a3f1fe68cace6cc4ee83260804188c3

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -24,7 +24,7 @@ void tst_cmd_p(){
    bool succ;
    char s[1000];
    FILE *fp=NULL;
-   fp=fopen("/ics2018/nemu/tools/gen-expr/input","r");
+   fp=fopen("/nemu/tools/gen-expr/input","r");
    while(!feof(fp))
    {
 	 memset(s,0,sizeof(s));
