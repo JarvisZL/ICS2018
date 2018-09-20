8e9b43d9ff83c2af2af22f216b6cb99552857e15
tracer-ics2018 <tracer@njuics.org>
2018-09-20 23:15:51 +0800

    >  compile 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-1 (2018-07-05) x86_64 GNU/Linux  23:15:51 up 18:46,  5 users,  load average: 0.22, 0.23, 0.22 4f77caccf2fcc86f58d9f1589ee05cd93670cc04

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -7,10 +7,9 @@ void tst();
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  int is_batch_mode = init_monitor(argc, argv);
-  
   tst();
-
+	
+  int is_batch_mode = init_monitor(argc, argv);
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
