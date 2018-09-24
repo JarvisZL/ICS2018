296414c22b33149f26355b0191ce5446e3b1ef8b
tracer-ics2018 <tracer@njuics.org>
2018-09-24 22:05:46 +0800

    >  compile 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-1 (2018-07-05) x86_64 GNU/Linux  22:05:46 up  1:33,  3 users,  load average: 0.54, 0.32, 0.25 5db400c25f015615c4c9a17bfa97e4ea9b9851b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -305,13 +305,14 @@ uint32_t eval(int p,int q,bool* LE)
 
 
 uint32_t expr(char *e, bool *success) {
+  init_regex();
   memset(tokens,0,sizeof(tokens));
   if_ze=false;
 
   if (!make_token(e)) {
     *success = false;
     if(*success==false)
-	    printf("make_token failed!");
+	    printf("make_token failed!\n");
     return 0;
   }
 
