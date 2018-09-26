4053a0ca42d6078018862606ac7bb254a4a37d3f
tracer-ics2018 <tracer@njuics.org>
2018-09-26 14:57:33 +0800

    >  compile 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-1 (2018-07-05) x86_64 GNU/Linux  14:57:33 up 11:41,  5 users,  load average: 0.41, 0.32, 0.20 891252243b23664fc5a312077f16950fcf7208

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -11,7 +11,7 @@ void init_wp_pool() {
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = &wp_pool[i + 1];
-    wp_pool[i].str="\0";
+   // wp_pool[i].str="\0";
   }
   wp_pool[NR_WP - 1].next = NULL;
   head = NULL;
@@ -129,7 +129,8 @@ bool checkwp()
 		   printf("old value: %d\n",tem->old_v);
 		   printf("new value: %d\n",tem->new_v);
 		   all=true;
-	   } 
+	   }
+	  tem=tem->next; 
 	}
 	return all;
 }
