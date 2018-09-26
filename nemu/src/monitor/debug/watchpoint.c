d970fe8dadf9113d0a0a96a628ce88884e449096
tracer-ics2018 <tracer@njuics.org>
2018-09-26 13:00:00 +0800

    >  compile 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-1 (2018-07-05) x86_64 GNU/Linux  13:00:00 up  9:43,  5 users,  load average: 0.60, 0.31, 0.19 93d2a7fce6c8028273dce348812adf332641ef95

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -11,9 +11,9 @@ void init_wp_pool() {
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = &wp_pool[i + 1];
+    wp_pool[i].str=NULL;
   }
   wp_pool[NR_WP - 1].next = NULL;
-
   head = NULL;
   free_ = wp_pool;
 }
@@ -31,14 +31,87 @@ WP* new_wp()
             tem=free_;
 	    free_=free_->next;
 	    tem->next=NULL;
-	    return tem;
+	    if(head==NULL)
+	    {
+		    head=tem;
+		    return tem;
+	    }
+	    else
+	    {
+		    tem->next=head;
+		    head=tem;
+		    return tem;
+	    }
 	}
 }
 
-void free_wp(WP* wp)
+WP* find_wp(char* s)
 {
-	wp->next=free_;
-	free_=wp;
+      if(head==NULL)
+      {
+            printf("no such a watchpoint!\n");
+	    return head;
+      }
+      else
+      {
+           tem=head;
+	   while(strcmp(tem->str,s)&&tem->next!=NULL)
+	   {
+		   tem=tem->next;
+	   }
+	   if(!strcmp(tem->str,s))
+	   {
+                  return tem;
+	   }
+	   else
+	   {
+                 return tem->next; 
+	   }
+      }
+}
+
+void free_wp(WP* wp)
+{       
+	if(wp==NULL)
+		return;
+	if(head==NULL)
+	{
+		printf("no using watchpoint!\n");
+	}
+        else if(head==wp)
+        {
+		if(wp->next==NULL)
+		{
+			head=NULL;
+			wp->next=free_;
+			free_=wp;
+		}
+		else
+		{
+                       head=head->next;
+		       wp->next=free_;
+		       free_=wp;
+		}
+	}
+	else
+	{
+              tem=head;
+	      while(tem->next!=wp&&tem->next!=NULL)
+	      {
+                      tem=tem->next;
+		      
+	      }
+	      if(tem->next==wp)
+	      {
+		      tem->next=wp->next;
+                      wp->next=free_;
+		      free_=wp;
+	      }
+	      if(tem->next==NULL)
+	      {
+		      printf("the watchpoint isn't using!\n");
+	      }
+	}
 }
 
 
