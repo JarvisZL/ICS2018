55ee5c6b4cf0b9c7ec4221c1a846d19d1f2b7de3
tracer-ics2018 <tracer@njuics.org>
2018-10-04 17:28:51 +0800

    >  compile 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:28:50 up  9:39,  7 users,  load average: 0.15, 0.03, 0.01 31f16ff76aead6a61fc57e47ea710b01b038f6db

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -217,6 +217,7 @@ static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   else cpu.EFLAGS.SF=1;
 }
 
+
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
   rtl_update_ZF(result, width);
   rtl_update_SF(result, width);
