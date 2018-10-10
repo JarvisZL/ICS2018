31add6e7c3d1c1d0801f6ff34d3d95ba60281480
tracer-ics2018 <tracer@njuics.org>
2018-10-10 20:59:16 +0800

    >  compile 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:59:16 up 22:45, 16 users,  load average: 0.24, 0.20, 0.30 d121f3e06d355bf86eb8a8f4a438686e13425156

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -108,7 +108,7 @@ void difftest_step(uint32_t eip) {
        nemu_state=NEMU_ABORT;
    }
    printflog("------------------------------serious sepatated line--------------------------------\n");
-   printflog("REGS:\teax\tecx\tedx\tebx\tesp\tebp\tesi\tedi\teip\n");
+   printflog("REGS:\teax       ecx\tedx\tebx\tesp\tebp\tesi\tedi\teip\n");
    printflog("QEMU:\t%-8x  %-8x  %-8x  %-8x  %-8x  %-8x  %-8x  %-8x  %-8x\n",ref_r.eax,ref_r.ecx,ref_r.edx,ref_r.ebx,ref_r.esp,ref_r.ebp,ref_r.esi,ref_r.edi,ref_r.eip);
   //TODO();
 }
