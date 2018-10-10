693961ecd8747903b4aa678d0dc9ae1397660dda
tracer-ics2018 <tracer@njuics.org>
2018-10-10 19:16:39 +0800

    >  compile 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:16:39 up 21:03, 13 users,  load average: 0.76, 0.33, 0.27 7c8e9b97832916edf1c7f855df7c5b28b5e4fc91

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -72,7 +72,7 @@ make_group(gp7,
 /* TODO: Add more instructions!!! */
 
 opcode_entry opcode_table [512] = {
-  /* 0x00 */	EMPTY, IDEX(G2E,add), EMPTY, EMPTY,
+  /* 0x00 */	EMPTY, IDEX(G2E,add), EMPTY, IDEX(E2G,add),
   /* 0x04 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x08 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x0c */	EMPTY, EMPTY, EMPTY, EX(2byte_esc),
@@ -139,7 +139,7 @@ opcode_entry opcode_table [512] = {
 
   /*2 byte_opcode_table */
 
-  /* 0x00 */	EMPTY, IDEX(gp7_E, gp7), EMPTY, IDEX(E2G,add),
+  /* 0x00 */	EMPTY, IDEX(gp7_E, gp7), EMPTY, EMPTY,
   /* 0x04 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x08 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x0c */	EMPTY, EMPTY, EMPTY, EMPTY,
