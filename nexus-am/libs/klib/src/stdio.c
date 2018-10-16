72432e24611cc025e7684424350114c198bb23d0
tracer-ics2018 <tracer@njuics.org>
2018-10-16 17:15:32 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:15:32 up  3:17, 10 users,  load average: 0.01, 0.02, 0.00 f94544a5b166027397b8be8e19b79896dcbb1fc4

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,6 +3,28 @@
 
 #ifndef __ISA_NATIVE__
 
+static int cnt;
+static void itos(char* s,int x)
+{
+    int a[20];
+    int num=0;
+    while(x/10!=0)
+    {
+        int y=x%10;
+        a[num++]=y;
+        x=x/10;
+    }
+    a[num]=x;
+   
+    for(int i=0;i<=num;i++)
+    {
+        *s=a[num-i]-'0';
+        s++;
+    }
+    *s='\0';
+    return;
+}
+
 int printf(const char *fmt, ...) {
   return 0;
 }
@@ -12,7 +34,69 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+    assert(fmt);
+    va_list ap;
+    va_start(ap,fmt);
+    cnt=0;
+   const char *str=fmt;
+    char *ptr=out;
+
+    while(*str!='\0')
+    {
+        if(*str=='%')
+        {
+            str++;
+            switch(*str)
+            {
+                case 's': {
+                              char *tem=va_arg(ap,char*);
+                              strcat(ptr,tem);
+                              cnt+=strlen(tem);
+                              break;
+                          }
+
+                case 'd': {
+                              int x=va_arg(ap,int);
+                              char *tem=NULL;
+                              char *ss=NULL;
+                              if(x==-2147483648)
+                              {
+                                  char temp[12]="-2147483648";
+                                  strcat(ptr,temp);
+                                  cnt+=strlen(temp);
+                              }
+                              else if(x<0)
+                              {
+                                  x=-x;
+                                  *ss='-';
+                                  tem=ss;
+                                  ss++;
+                                  itos(ss,x);
+                                  strcat(ptr,tem);
+                                  cnt+=strlen(tem);
+                              }
+                              else if(x>=0)
+                              {
+                                  tem=ss;
+                                  itos(ss,x);
+                                  strcat(ptr,tem);
+                                  cnt+=strlen(tem);
+                              }
+                              break;
+                          }
+            }
+        }
+      else
+      {
+          char *tem=NULL;
+          *tem=*str;
+          *(tem+1)='\0';
+          strcat(ptr,tem);
+          cnt++;
+      }
+    }
+    va_end(ap);
+    return cnt;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
