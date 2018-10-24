11cc210e8a4b2b23697a9fd825c3ee267a2f3b71
tracer-ics2018 <tracer@njuics.org>
2018-10-24 20:20:40 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:20:40 up  5:14, 10 users,  load average: 0.21, 0.13, 0.05 b1dee4289b6ed95a9474c064e177a9fb26cb7f

--- nexus-am/am/arch/x86-nemu/src/devices/timer.c
+++ nexus-am/am/arch/x86-nemu/src/devices/timer.c
@@ -2,12 +2,14 @@
 #include <x86.h>
 #include <amdev.h>
 
+extern uint32_t uptime();
 size_t timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
+      uint32_t tem=uptime(); 
       _UptimeReg *uptime = (_UptimeReg *)buf;
       uptime->hi = 0;
-      uptime->lo = 0;
+      uptime->lo = tem;
       return sizeof(_UptimeReg);
     }
     case _DEVREG_TIMER_DATE: {
