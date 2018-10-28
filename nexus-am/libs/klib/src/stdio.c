e14d5679071cdb80b69ab7401d8f3f401f7e8053
tracer-ics2018 <tracer@njuics.org>
2018-10-28 16:16:14 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:16:14 up  8:56, 15 users,  load average: 0.00, 0.00, 0.02 3ca2efb5ef336a45f780f669cadeed2f60d8c28f

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -5,6 +5,8 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 static int cnt;
+static int cntvs;
+static int cnts;
 
 static void itos(char s[],int x)
 {
@@ -25,7 +27,7 @@ static void itos(char s[],int x)
     s[num+1]='\0';
     return;
 }
-
+/*
 int printf(const char *fmt, ...) {
     assert(fmt);
     va_list ap;
@@ -115,9 +117,95 @@ int printf(const char *fmt, ...) {
     va_end(ap);
     return cnt;
 }
+*/
+
+int printf(const char *fmt, ...) {
+    assert(fmt);
+    va_list ap;
+    va_start(ap,fmt);
+    char s[200];
+    cnt=vsprintf(s,fmt,ap);
+    for(int i=0;;i++)
+    {
+        if(s[i]=='\0')
+            break;
+        _putc(s[i]);
+    }
+    va_end(ap);
+    return cnt;
+}
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+    assert(fmt);
+    cntvs=0;
+    const char *str=fmt;
+    char *tem=NULL;
+    char temp[50];
+    char s[50];
+    int x;
+    memset(out,0,sizeof(out));
+    while((*str)!='\0')
+    {
+        if(*str=='%')
+        {
+            str++;
+            switch(*str)
+            {
+                case 's':{
+                             tem=va_arg(ap,char*);
+                             strcat(out,tem);
+                             cntvs+=strlen(tem);
+                             break;
+                         }
+                case 'd':{
+                             memset(s,0,sizeof(s));
+                             memset(temp,0,sizeof(temp));
+                             x=va_arg(ap,int);
+                             if(x==-2147483648)
+                             {
+                                 strcpy(temp,"-2147483648");
+                                 strcat(out,temp);
+                                 cntvs+=strlen(temp);
+                             }
+                             else if(x<0)
+                             {
+                                 x=-x;
+                                 temp[0]='-';
+                                 itos(s,x);
+                                 strcat(temp,s);
+                                 strcat(out,temp);
+                                 cntvs+=strlen(temp);
+                             }
+                             else
+                             {
+                                 itos(s,x);
+                                 strcat(out,s);
+                                 cntvs+=strlen(s);
+                             }    
+                             break;
+                         }
+                case 'c':{
+                            tem=va_arg(ap,char*);
+                            assert(strlen(tem)==1);
+                            strcat(out,tem);
+                            cntvs++;
+                            break;
+                         }
+            }
+            str++;
+        }
+        else
+        {
+            char ss[2];
+            memset(ss,0,sizeof(ss));
+            ss[0]=*str;
+            ss[1]='\0';
+            strcat(out,ss);
+            cntvs++;
+            str++;
+        }
+    } 
+    return cntvs;
 }
 
 
@@ -125,7 +213,7 @@ int sprintf(char *out, const char *fmt,...){
     assert(fmt);
     va_list ap;
     va_start(ap,fmt);
-    cnt=0;
+    cnts=0;
     const char *str=fmt;
     char *tem=NULL;
     char temp[50];
@@ -143,7 +231,7 @@ int sprintf(char *out, const char *fmt,...){
                 case 's':{
                             tem=va_arg(ap,char*);
                             strcat(out,tem);
-                            cnt+=strlen(tem);
+                            cnts+=strlen(tem);
                             break;
                          }
                         
@@ -155,7 +243,7 @@ int sprintf(char *out, const char *fmt,...){
                                {
                                     strcpy(temp,"-2147483648");
                                     strcat(out,temp);
-                                    cnt+=strlen(temp);
+                                    cnts+=strlen(temp);
                                }
                                else if(x<0)
                                {
@@ -164,14 +252,14 @@ int sprintf(char *out, const char *fmt,...){
                                   itos(s,x);
                                   strcat(temp,s);
                                   strcat(out,temp);
-                                  cnt+=strlen(temp);
+                                  cnts+=strlen(temp);
                                  
                                }
                                else 
                                {
                                    itos(s,x);
                                    strcat(out,s);
-                                   cnt+=strlen(s);
+                                   cnts+=strlen(s);
                                }
                                break;
                           }
@@ -184,13 +272,13 @@ int sprintf(char *out, const char *fmt,...){
            temp[0]=*str;
            temp[1]='\0';
            strcat(out,temp);
-           cnt++;
+           cnts++;
            str++;
         }
             
     }
     va_end(ap);
-    return cnt;
+    return cnts;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
