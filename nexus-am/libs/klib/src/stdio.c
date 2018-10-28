3b1af0f615d2da5fc78d1f366af114d739494543
tracer-ics2018 <tracer@njuics.org>
2018-10-28 17:04:11 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:04:11 up  9:44, 15 users,  load average: 0.09, 0.10, 0.06 9b12541f38493512c80fa150c6ae3acb56456047

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -1,5 +1,7 @@
 #include "klib.h"
 #include <stdarg.h>
+#include <stdlib.h>
+
 
 //#ifndef __ISA_NATIVE__
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
@@ -26,6 +28,17 @@ static void itos(char s[],int x)
     }
     s[num+1]='\0';
     return;
+}
+
+static void stoi(char s[],int n)
+{
+   int index=0;
+   while(s[index]!='\0')
+   {
+       n=n*10+s[index]-'0';
+       index++;
+   }
+   return;
 }
 /*
 int printf(const char *fmt, ...) {
@@ -143,18 +156,79 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
     char temp[50];
     char s[50];
     int x;
+    int flag=0;
     memset(out,0,sizeof(out));
+
+    char num[20];
+    int k,n=0;
+
     while((*str)!='\0')
     {
         if(*str=='%')
         {
             str++;
+            while(*str==48)
+            {
+                flag=1;
+                str++;
+            }
+            k=0;
+            memset(num,0,sizeof(num));
+            while(*str>48&&*str<=57)
+            {
+               num[k]=*str;
+               k++;
+               str++;
+            }
+            if(num[0]!='\0')
+            {
+                stoi(num,n);
+            }
+
+
+
             switch(*str)
             {
                 case 's':{
                              tem=va_arg(ap,char*);
-                             strcat(out,tem);
-                             cntvs+=strlen(tem);
+                             if(flag)
+                             {
+                                if(strlen(tem)>=n)
+                                {
+                                   strcat(out,tem);
+                                   cntvs+=strlen(tem);
+                                }
+                                else
+                                {
+                                    char s1[10]="";
+                                    for(int i=0;i<n-strlen(tem);i++)
+                                        s1[i]='0';
+                                    s1[n-strlen(tem)]='\0';
+                                    strcat(out,s1);
+                                    cntvs+=strlen(s1);
+                                    strcat(out,tem);
+                                    cntvs+=strlen(tem);
+                                }
+                             }
+                             else
+                             {
+                                if(strlen(tem)>=n)
+                                {
+                                   strcat(out,tem);
+                                   cntvs+=strlen(tem);
+                                }
+                                else
+                                {
+                                    char s1[10]="";
+                                    for(int i=0;i<n-strlen(tem);i++)
+                                        s1[i]=' ';
+                                    s1[n-strlen(tem)]='\0';
+                                    strcat(out,s1);
+                                    cntvs+=strlen(s1);
+                                    strcat(out,tem);
+                                    cntvs+=strlen(tem);
+                                }
+                             }
                              break;
                          }
                 case 'd':{
@@ -171,16 +245,90 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
                              {
                                  x=-x;
                                  temp[0]='-';
-                                 itos(s,x);
-                                 strcat(temp,s);
                                  strcat(out,temp);
-                                 cntvs+=strlen(temp);
+                                 cntvs++;
+                                 n--;
+                                 itos(s,x);
+                                 if(flag)
+                                 {
+                                     if(strlen(s)>=n)
+                                     {
+                                         strcat(out,s);
+                                         cntvs+=strlen(s);
+                                     }
+                                     else
+                                     {
+                                        char s1[10]="";
+                                        for(int i=0;i<n-strlen(s);i++)
+                                            s1[i]='0';
+                                        s1[n-strlen(s)]='\0';
+                                        strcat(out,s1);
+                                        cntvs+=strlen(s1);
+                                        strcat(out,s);
+                                        cntvs+=strlen(s);
+                                     }
+                                 }
+                                 else
+                                 {
+                                     if(strlen(s)>=n)
+                                     {
+                                         strcat(out,s);
+                                         cntvs+=strlen(s);
+                                     }
+                                     else
+                                     {
+                                        char s1[10]="";
+                                        for(int i=0;i<n-strlen(s);i++)
+                                            s1[i]=' ';
+                                        s1[n-strlen(s)]='\0';
+                                        strcat(out,s1);
+                                        cntvs+=strlen(s1);
+                                        strcat(out,s);
+                                        cntvs+=strlen(s);
+                                     }
+                                 }
                              }
                              else
                              {
                                  itos(s,x);
-                                 strcat(out,s);
-                                 cntvs+=strlen(s);
+                                 if(flag)
+                                 {
+                                     if(strlen(s)>=n)
+                                     {
+                                         strcat(out,s);
+                                         cntvs+=strlen(s);
+                                     }
+                                     else
+                                     {
+                                        char s1[10]="";
+                                        for(int i=0;i<n-strlen(s);i++)
+                                            s1[i]='0';
+                                        s1[n-strlen(s)]='\0';
+                                        strcat(out,s1);
+                                        cntvs+=strlen(s1);
+                                        strcat(out,s);
+                                        cntvs+=strlen(s);
+                                     }
+                                 }
+                                 else
+                                 {
+                                     if(strlen(s)>=n)
+                                     {
+                                         strcat(out,s);
+                                         cntvs+=strlen(s);
+                                     }
+                                     else
+                                     {
+                                        char s1[10]="";
+                                        for(int i=0;i<n-strlen(s);i++)
+                                            s1[i]=' ';
+                                        s1[n-strlen(s)]='\0';
+                                        strcat(out,s1);
+                                        cntvs+=strlen(s1);
+                                        strcat(out,s);
+                                        cntvs+=strlen(s);
+                                     }
+                                 }
                              }    
                              break;
                          }
