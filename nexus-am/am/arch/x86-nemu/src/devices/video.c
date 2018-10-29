1730128186ecde70466f0b23868db599026c23e2
tracer-ics2018 <tracer@njuics.org>
2018-10-29 14:39:05 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:39:05 up  1:18,  8 users,  load average: 0.06, 0.17, 0.14 2dd74d375470d39e7c6cbe60ed732d2887aac4fe

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -40,7 +40,7 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
       int x=ctl->x, y=ctl->y, w=ctl->w, h=ctl->h;
       uint32_t *pixels=ctl->pixels;
       int cp_bytes=sizeof(uint32_t) *min(w,SCREEN_W-x);
-      for(int j=0;j<h&&y+j<SCREEN_H;j++){
+      for(int j=0;j<h&&(y+j)<SCREEN_H;j++){
           memcpy(&fb[(y+j)*SCREEN_W+x],pixels,cp_bytes);
           pixels+=w;
       } 
