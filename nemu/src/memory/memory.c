4f7b77e0c7147837e3951f1140fb2c494ed924e1
tracer-ics2018 <tracer@njuics.org>
2018-10-29 14:08:58 +0800

    >  compile 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:08:58 up 48 min,  8 users,  load average: 0.13, 0.12, 0.09 29d6f3f726cf599995467aa7e4358d8853e9ac2

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -11,13 +11,34 @@ uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
 
+extern int is_mmio(paddr_t addr);
+extern uint32_t mmio_read(paddr_t addr,int len,int map_NO);
+extern void mmio_write(paddr_t addr,int len,uint32_t data,int map_NO);
+
+/*
 uint32_t paddr_read(paddr_t addr, int len) {
   return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
+*/
+uint32_t paddr_read(paddr_t addr, int len) {
+     if(is_mmio(addr)!=-1)
+        return mmio_read(addr,len,is_mmio(addr)); 
+     else
+        return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+}
 
+/*
 void paddr_write(paddr_t addr, uint32_t data, int len) {
   memcpy(guest_to_host(addr), &data, len);
 }
+*/
+void paddr_write(paddr_t addr, uint32_t data, int len) {
+    if(is_mmio(addr)!=-1)
+        mmio_write(addr,len,data,is_mmio(addr));
+    else
+        memcpy(guest_to_host(addr), &data, len);
+}
+
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
   return paddr_read(addr, len);
