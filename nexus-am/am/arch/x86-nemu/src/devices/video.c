0ff622a75b6b153bb8074609303bbdbb679c0d0a
tracer-ics2018 <tracer@njuics.org>
2018-10-29 14:45:59 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:45:58 up  1:25,  8 users,  load average: 0.10, 0.28, 0.24 a4590a72841f7253d0e4a84b29a27e6cc7c0336

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -20,8 +20,8 @@ size_t video_read(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_VIDEO_INFO: {
       info1=inl(SCREEN_PORT);
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      info->width = 200;//((info1>>16)&0xffff);//-((info0>>16)&0xffff);
-      info->height = 300;//(info1&0xffff);//-(info0&0xffff);
+      info->width = ((info1>>16)&0xffff);//-((info0>>16)&0xffff);
+      info->height =(info1&0xffff);//-(info0&0xffff);
       return sizeof(_VideoInfoReg);
     }
   }
@@ -32,11 +32,11 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
-
+    /*
       int i;
       int size =screen_width()*screen_height();
       for(i=0; i<size; i++) fb[i]=i;
-      
+      */
       int x=ctl->x, y=ctl->y, w=ctl->w, h=ctl->h;
       uint32_t *pixels=ctl->pixels;
       int cp_bytes=sizeof(uint32_t) *min(w,SCREEN_W-x);
