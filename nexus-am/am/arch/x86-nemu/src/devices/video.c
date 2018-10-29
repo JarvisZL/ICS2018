8d2bc26c1cef0a3421755ff844e49131908240eb
tracer-ics2018 <tracer@njuics.org>
2018-10-29 14:33:04 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:33:04 up  1:12,  8 users,  load average: 0.00, 0.08, 0.10 c746bce2dd6160f02374cf77abda4ab9c6c45581

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -3,10 +3,17 @@
 #include <amdev.h>
 #include <klib.h>
 #define SCREEN_PORT 0x100
+#define SCREEN_W 400
+#define SCREEN_H 300
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 static uint32_t info0;
 static uint32_t info1;
+
+static inline int min(int x,int y)
+{
+    return x<y ? x:y;
+}
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
@@ -28,7 +35,15 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
 
       int i;
       int size =screen_width()*screen_height();
-      for(i=0; i<size; i++) fb[i]=i; 
+      for(i=0; i<size; i++) fb[i]=i;
+      
+      int x=ctl->x, y=ctl->y, w=ctl->w, h=ctl->h;
+      uint32_t *pixels=ctl->pixels;
+      int cp_bytes=sizeof(uint32_t) *min(w,SCREEN_W-x);
+      for(int j=0;j<h&&y+j<SCREEN_H;j++){
+          memcpy(&fb[(y+j)*SCREEN_W+x],pixels,cp_bytes);
+          pixels+=w;
+      } 
       
       if (ctl->sync) {
         // do nothing, hardware syncs.
