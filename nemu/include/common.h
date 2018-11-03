50b1bc2ee2a8ae098467d0dfa02148898f1e780b
tracer-ics2018 <tracer@njuics.org>
2018-11-03 15:31:58 +0800

    >  compile 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:31:58 up  6:39,  6 users,  load average: 0.83, 0.26, 0.09 85dc4f877f39f13d7d08ba650d36236356d4801

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -1,10 +1,10 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define DEBUG
-#define DIFF_TEST
+//#define DEBUG
+//#define DIFF_TEST
 
-volatile static int dummy;
+//volatile static int dummy;
 #if _SHARE
 // do not enable these features while building a reference design
 #undef DIFF_TEST
