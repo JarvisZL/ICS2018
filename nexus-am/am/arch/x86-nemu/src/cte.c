580fcab3b27a6d8551d3b437a4cde5e26b7f8330
tracer-ics2018 <tracer@njuics.org>
2018-11-13 19:46:55 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:46:55 up  7:06, 28 users,  load average: 0.42, 0.57, 0.49 1a094045b8edf351bdb13eac7d3bec278c5db7f

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <x86.h>
-#include <stdio.h>
+#include <klib.h>
 static _Context* (*user_handler)(_Event, _Context*) = NULL;
 
 void vectrap();
@@ -8,7 +8,7 @@ void vecnull();
 
 _Context* irq_handle(_Context *tf) {
     //debug
-//    printf("%d  %d  %d  %d  %d",tf->eax,tf->ecx,tf->edx,tf->ebx,tf->esp);
+   printf("%d  %d  %d  %d  %d",tf->eax,tf->ecx,tf->edx,tf->ebx,tf->esp);
   _Context *next = tf;
   if (user_handler) {
     _Event ev;
