f62ccadba2a1e8489fcc1bf5f521696127bb0b27
tracer-ics2018 <tracer@njuics.org>
2018-11-18 14:26:36 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:26:36 up 15:49, 25 users,  load average: 0.06, 0.07, 0.07 3d134cb8b9cea018a1f9d950f4fa045b59c50826

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,7 +10,6 @@ static void out_write(uintptr_t index,uintptr_t len)
     char *ptr=(char *)index;
     for(uintptr_t i=0;i<len;++i)
          _putc(ptr[i]);
-
 }
 
 
@@ -22,7 +21,7 @@ _Context* do_syscall(_Context *c) {
   a[3] = c->GPR4;
 
   switch (a[0]) {
-      case SYS_write: if(a[1]==1||a[1]==2){ out_write(a[2],a[3]); c->GPRx=a[3]; } else c->GPRx=-1; break;
+      case SYS_write: if(a[1]==1||a[1]==2){ out_write(a[2],a[3]); c->GPRx=c->GPR4; } else c->GPRx=-1; break;
     case SYS_exit: _halt(c->GPR2); break;
     case SYS_yield: _yield(); c->GPRx=0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
