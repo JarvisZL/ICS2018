882c1d30de72807f33ccd64990074fdd3fb1b25f
tracer-ics2018 <tracer@njuics.org>
2018-11-18 14:16:32 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:16:32 up 15:39, 25 users,  load average: 0.09, 0.05, 0.05 ade8d305381b827a3c981b595009edc324766cb8

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,6 +4,16 @@
 void _yield();
 void _halt(int code);
 
+
+static void out_write(uintptr_t index,uintptr_t len)
+{
+    char *ptr=(char *)index;
+    for(uintptr_t i=0;i<len;++i)
+         _putc(ptr[i]);
+
+}
+
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -12,22 +22,7 @@ _Context* do_syscall(_Context *c) {
   a[3] = c->GPR4;
 
   switch (a[0]) {
-      case SYS_write: {
-                          if(a[1]==1||a[1]==2)
-                          {
-                             char * ptr=(char *)a[2];
-                             for(int i=0;i<a[3];++i)
-                             {
-                                  _putc(*ptr);
-                                  ptr++;
-                             }
-                             c->GPRx=a[3];
-                          }
-                          else
-                            c->GPRx=-1;
-                          break;
-                      }
-
+      case SYS_write: if(a[1]==1||a[1]==2){ out_write(a[2],a[3]); c->GPRx=a[3]; } else c->GPRx=-1; break;
     case SYS_exit: _halt(c->GPR2); break;
     case SYS_yield: _yield(); c->GPRx=0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
@@ -35,3 +30,4 @@ _Context* do_syscall(_Context *c) {
 
   return NULL;
 }
+
