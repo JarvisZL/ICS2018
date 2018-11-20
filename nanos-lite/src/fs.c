88f30d257c06b5de39094660c091628ccf8b5fb9
tracer-ics2018 <tracer@njuics.org>
2018-11-20 21:31:06 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:31:06 up 1 day, 23 min, 51 users,  load average: 0.05, 0.26, 0.45 38ba02cf8bb3b800d4241ae5f07f92da5228a390

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -103,6 +103,7 @@ ssize_t fs_read(int fd,void *buf,size_t len)
    {
        Log("len:%d\n",len);
        len=file_table[fd].size-file_table[fd].open_offset;
+       Log("len:%d\n",len);
    }
    ret_val=ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len); 
    file_table[fd].open_offset+=ret_val;
