b245ecc0cef100f812fd35d7360823a64f63abb4
tracer-ics2018 <tracer@njuics.org>
2018-11-20 21:24:19 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:24:19 up 1 day, 17 min, 51 users,  load average: 0.68, 0.63, 0.64 ffb861d0edfb95bd9a46a19914239254b2bc4c2f

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -95,12 +95,12 @@ off_t fs_lseek(int fd,off_t offset,int whence)
 static ssize_t ret_val; 
 ssize_t fs_read(int fd,void *buf,size_t len)
 {
-   if(file_table[fd].open_offset==file_table[fd].size&&len!=0)
+   if(file_table[fd].open_offset==file_table[fd].size-1&&len!=0)
          return -1;
    if(len==0)
          return 0;
-   if(file_table[fd].open_offset+len>file_table[fd].size)
-       len=file_table[fd].size-3-file_table[fd].open_offset;
+   if(file_table[fd].open_offset+len>file_table[fd].size-1)
+       len=file_table[fd].size-1-file_table[fd].open_offset;
    ret_val=ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len); 
    file_table[fd].open_offset+=ret_val;
    return ret_val;
