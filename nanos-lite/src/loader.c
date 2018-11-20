c9d10209d6391cb5f370630a08c7b0c79a1503f2
tracer-ics2018 <tracer@njuics.org>
2018-11-20 17:21:57 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:21:57 up 21:29, 23 users,  load average: 0.09, 0.12, 0.19 5c4f57cd6d94ee7bbc0dc154b481737966cf656d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -21,7 +21,6 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     buff=(void *)DEFAULT_ENTRY;
     fs_read(fd,buff,length);
     // ramdisk_read(buff,0,length);
-  
     
     // TODO();
   return DEFAULT_ENTRY;
