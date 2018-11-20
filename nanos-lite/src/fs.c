3a53b6fe67ad31b411b0a50b592a2ff636a224ff
tracer-ics2018 <tracer@njuics.org>
2018-11-20 17:47:50 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:47:50 up 21:55, 25 users,  load average: 0.26, 0.31, 0.30 e6df948c10836d1a80c4282f2c58861ed4a9351

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -77,7 +77,7 @@ off_t fs_lseek(int fd,off_t offset,int whence)
        case SEEK_SET: {//SEEK_SET
                    assert(offset<=file_table[fd].size);
                    file_table[fd].open_offset=offset;
-                   return offset;
+                   return file_table[fd].open_offset;
                }
        case SEEK_CUR: {//SEEK_CUR
                    assert(file_table[fd].open_offset+offset<=file_table[fd].size);
