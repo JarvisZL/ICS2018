d6076fadf698f17eb5d4505847eba2de8e3490de
tracer-ics2018 <tracer@njuics.org>
2018-11-20 19:08:14 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:08:13 up 22:48, 34 users,  load average: 0.00, 0.14, 0.21 e545fdae6c2262549a0d40ff69debab7a963561c

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -75,6 +75,7 @@ off_t fs_lseek(int fd,off_t offset,int whence)
    switch(whence)
    {
        case SEEK_SET: {Log("this is SET");
+                      Log("size:%d",file_table[fd].size);
                    assert(offset<=file_table[fd].size);
                    file_table[fd].open_offset=offset;
                    return file_table[fd].open_offset;
