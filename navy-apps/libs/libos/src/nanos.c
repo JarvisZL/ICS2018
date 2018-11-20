9d1fe26f5f771dc5d36fcc71105345fb9a7b2568
tracer-ics2018 <tracer@njuics.org>
2018-11-20 22:24:04 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:24:04 up 1 day,  1:16, 51 users,  load average: 0.34, 0.21, 0.29 c423f4bdf0be92487f4af1eacb2bfca9b790abd7

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -36,7 +36,7 @@ int _open(const char *path, int flags, mode_t mode) {
 int _write(int fd, void *buf, size_t count){
   _syscall_(SYS_write,fd,(intptr_t)buf,count);
   // _exit(SYS_write);
- return count;
+ return count;//because stdout and stderror dont have ret;
 }
 
 
