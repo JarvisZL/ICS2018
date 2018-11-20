28c9eff142a697954f6d543e790b3b7da16cd44f
tracer-ics2018 <tracer@njuics.org>
2018-11-20 22:11:42 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:11:42 up 1 day,  1:04, 51 users,  load average: 0.28, 0.38, 0.49 aa6ebed13267623f67efd258d56b13effb8863

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -42,7 +42,7 @@ _Context* do_syscall(_Context *c) {
     case SYS_open: c->GPRx=fs_open((const char*)a[1],a[2],a[2]); break;
     case SYS_read: c->GPRx=fs_read(a[1],(void *)a[2],(size_t)a[3]); break;
     case SYS_brk:  c->GPRx=brk((void *)a[1]); break;    
-    case SYS_write: if(a[1]==1||a[1]==2){ out_write(a[2],a[3]); /*Log("call!");*/}else c->GPRx=fs_write(a[1],(void*)a[2],(size_t)a[3]);  break;
+    case SYS_write: if(a[1]==1||a[1]==2){ out_write(a[2],a[3]); /*Log("call!");*/}else if(a[1]!=1&&a[1]!=2&&a[1]!=3) c->GPRx=fs_write(a[1],(void*)a[2],(size_t)a[3]);  break;
     case SYS_exit: _halt(c->GPR2); break;
     case SYS_yield: _yield(); c->GPRx=0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
