335fd43528a1ea7074436e7099f63b6396970dee
tracer-ics2018 <tracer@njuics.org>
2018-11-20 19:26:59 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:26:59 up 23:07, 34 users,  load average: 0.32, 0.33, 0.27 311acedf2a827f089875483b67c2112b794d2ba

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -5,15 +5,11 @@ int main() {
   FILE *fp = fopen("/share/texts/num", "r+");
   assert(fp);
 
-
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
 
-
   fseek(fp, 500 * 5, SEEK_SET);
-  printf("%p\n",fp);
-  printf("1\n");
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
@@ -21,7 +17,6 @@ int main() {
     assert(n == i + 1);
   }
 
-  printf("3\n");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
