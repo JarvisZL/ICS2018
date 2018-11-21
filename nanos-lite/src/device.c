877038b75958e996129a9b663a5425c890a795bc
tracer-ics2018 <tracer@njuics.org>
2018-11-21 22:37:09 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:37:09 up  3:49, 11 users,  load average: 0.09, 0.10, 0.07 d48faa3ba8a51ff0eda735d58df36803f5993cb

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -26,6 +26,7 @@ static char dispinfo[128] __attribute__((used));
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
     assert(offset<=128);
+    Log("%d",len);
     if(offset+len>128)
         len=128-offset;
     Log("%d",len);
