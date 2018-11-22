72d030877e79f6b52b0d40fc87aea8b603bb7f81
tracer-ics2018 <tracer@njuics.org>
2018-11-22 21:09:22 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:09:22 up 10:39, 19 users,  load average: 1.22, 0.56, 0.39 30bf1f5801147af8be7eaad96657eba9f13564

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -1,20 +1,15 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <ndl.h>
-#include <stdio.h>
+
 int main() {
   NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
   assert(bmp->pixels);
-  printf("hello\n");
   NDL_OpenDisplay(bmp->w, bmp->h);
-  printf("hello1\n");
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
-  printf("hello2\n");
   NDL_Render();
-  printf("hello3\n");
   NDL_CloseDisplay();
-  printf("hello4\n");
   while (1);
   return 0;
 }
