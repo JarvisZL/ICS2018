77d0409ff39238005872a03fec63d0b2e37aea50
tracer-ics2018 <tracer@njuics.org>
2018-11-22 20:30:46 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:30:46 up 10:00, 14 users,  load average: 0.46, 0.44, 0.76 31d981df531053f121d1e2e7fba519f847fc9a65

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -124,6 +124,7 @@ ssize_t fs_read(int fd,void *buf,size_t len)
    }
    Log("ret_read:%d",ret_read);
    file_table[fd].open_offset+=ret_read;
+   Log("openoffset:%d",file_table[fd].open_offset);
    return ret_read;
 }
 
