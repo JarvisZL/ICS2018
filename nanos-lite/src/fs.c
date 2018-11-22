7fa25845f08d9df7e02ed927c070cb32e31faf5d
tracer-ics2018 <tracer@njuics.org>
2018-11-22 20:53:38 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:53:38 up 10:23, 16 users,  load average: 0.20, 0.33, 0.39 cb7db7b9d4085656685cf90430b2ab571bdaad21

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -57,6 +57,7 @@ extern size_t ramdisk_write(const void* buf,size_t offset,size_t len);
 
 int fs_open(const char *pathname,int flags,int mode)
 {
+     Log("%s",pathname);
      static int index=0;
      for(index=0;index<NR_FILES;++index)
      {
