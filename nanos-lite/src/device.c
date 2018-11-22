b56ec57fd05eb7e6f71dcafa3fde1a1b3c52f390
tracer-ics2018 <tracer@njuics.org>
2018-11-22 19:26:21 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:26:21 up  9:32, 14 users,  load average: 1.63, 1.15, 1.05 2b0a33f05f6eaf4c618b4e9a6a58d812e899b609

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -46,7 +46,7 @@ extern ssize_t fs_read(int fd,void * buf,size_t len);
 void init_device() {
   Log("Initializing devices...");
   _ioe_init();
-   sprintf(dispinfo,"WIDTH:%d\nHEIGHT:%d",screen_width,screen_height); 
+   sprintf(dispinfo,"WIDTH:%d\nHEIGHT:%d",screen_width(),screen_height()); 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
 }
