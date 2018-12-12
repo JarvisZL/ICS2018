3c3d803c0c956701a42bbfc257f96acbb6222a4e
tracer-ics2018 <tracer@njuics.org>
2018-12-12 17:22:56 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:22:56 up 12:33, 33 users,  load average: 0.18, 0.22, 0.16 f753cff96fd959cb6a81503910c53009eb278de9

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,8 +26,8 @@ extern void context_kload(PCB *pcb, void *entry);
 
 void init_proc() {
     naive_uload(NULL, "/bin/init");
-    context_kload(&pcb[0],(void*) hello_fun);
     assert(0);
+    context_kload(&pcb[0],(void*) hello_fun);
     switch_boot_pcb();
 }
 
