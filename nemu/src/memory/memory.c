cb0e181fe18aa0f07f50a4c595d9eed11e013320
tracer-ics2018 <tracer@njuics.org>
2018-12-14 18:00:46 +0800

    >  compile 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  18:00:46 up  2:50, 11 users,  load average: 0.23, 0.20, 0.47 f36f7cf1dafda8fb429322ac7d14a21ab432115c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -49,20 +49,20 @@ paddr_t page_translate(vaddr_t addr)
     else
     {
         Log("page on");
-        Log("vaddr:%x",addr);
+      //  Log("vaddr:%x",addr);
         unsigned long dir=(unsigned long)(addr>>22)<<2;
-        Log("cpu.cr3:%x",cpu.cr3.val);
+       // Log("cpu.cr3:%x",cpu.cr3.val);
         unsigned long base=cpu.cr3.page_directory_base<<12;
         PDE * pde_p=(PDE *) (base|dir);
-        Log("dir:%lx,base:%lx",dir,base);
-        Log("addr:%lx",(uintptr_t) pde_p);
-        assert(pde_p->pde.present);
+       // Log("dir:%lx,base:%lx",dir,base);
+        //Log("addr:%lx",(uintptr_t) pde_p);
+        assert(pde_p->present);
         /*if(pde_p->present==0)
         {
             assert(0);
         }*/
         assert(0);
-      /*  unsigned long sec_base=pde_p->page_frame<<12;
+        unsigned long sec_base=pde_p->page_frame<<12;
         unsigned long page=((addr>>12)<<22)>>20;
         PTE * pte_p=(PTE*) (sec_base|page);
         if(pte_p->present==0)
@@ -73,7 +73,7 @@ paddr_t page_translate(vaddr_t addr)
         unsigned long offset=addr&0xfff;
         assert(0);
         return (paddr_t) (page_base|offset);
-*/        
+        
        // assert(0);
     }
 }
