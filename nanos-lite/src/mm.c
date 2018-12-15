af5d1f35a9cb17a3aea0c09b386b36f9f02d2a62
tracer-ics2018 <tracer@njuics.org>
2018-12-15 14:49:39 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:49:39 up  5:13, 12 users,  load average: 0.30, 0.10, 0.04 b3ddf326e74efe2482f5ed91e62e87a1549eeecd

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -19,6 +19,8 @@ int _map(_Protect *p,void *va,void* pa,int mode);
 /* The brk() system call handler. */
 int mm_brk(uintptr_t new_brk) {
    
+    Log("malloc");
+    Log("malloc");
     if(new_brk > current->max_brk)
     {
        uintptr_t above=new_brk-current->max_brk;
