de203d36a57fda1b0704ac1666b7477cb98563ca
tracer-ics2018 <tracer@njuics.org>
2018-12-18 09:32:31 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  09:32:31 up  4:01,  8 users,  load average: 0.18, 0.13, 0.10 1270b8bc23baaaa47dd764627b0cc1b3d09ff83b

--- nexus-am/am/arch/x86-navy/src/trm.c
+++ nexus-am/am/arch/x86-navy/src/trm.c
@@ -1,12 +1,16 @@
 #include <am.h>
 
 void _trm_init() {
+  int ret=main();
+  _halt(ret);
 }
 
 void _putc(char ch) {
 }
 
 void _halt(int code) {
+  __asm__ volatile(".byte 0xd6": :"a"(code));
+    while(1);
 }
 
 _Area _heap;
