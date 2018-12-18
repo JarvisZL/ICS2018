4d43591d8064c5b98891b4921d7cc8fa7b0b64b4
tracer-ics2018 <tracer@njuics.org>
2018-12-18 10:04:24 +0800

    >  run 171240524 jarviszly Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:04:24 up  4:33,  8 users,  load average: 1.82, 1.80, 1.19 c19fcd3fdd5f23549af0f872fa7c8123e9f831

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,8 +30,8 @@ extern void context_uload(PCB *pcb,const char *filename);
 void init_proc() {
   //  context_kload(&pcb[0],(void*) hello_fun);
     context_uload(&pcb[0],"/bin/hello");
-    context_uload(&pcb[1],"/bin/init");
-    context_uload(&pcb[2],"/bin/init");
+    context_uload(&pcb[1],"/bin/pal");
+    context_uload(&pcb[2],"/bin/slider");
     context_uload(&pcb[3],"/bin/init");
     for(int i=0;i<MAX_NR_PROC;++i)
         s_pcb[i]=pcb[i];
